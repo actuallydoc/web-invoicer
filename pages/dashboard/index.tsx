f6eb0f3534d613835667fc10089520b39db7ba47
Maj Knez <actuallydoc@gmail.com>
2023-02-12 21:58:05 +0100

    Experimenting with tables
    React-Table trying some stuff xd...

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Navbar from "@/components/dashboard/Navbar";
+import Table from "@/components/dashboard/Table";
 
 const Index = () => {
     return (
         <div>
-            sda
+            <header>
+                <title>Nadzorna plošča</title>
+                <Navbar />
+            </header>
+            <main>
+                <h1>Tabla</h1>
+                <Table />
+            </main>
+
         </div>
     );
 };
