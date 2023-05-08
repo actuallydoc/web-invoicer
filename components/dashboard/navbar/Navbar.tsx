33581b53ec134c696c81e0a8324999893e8ad3d2
Maj Knez <actuallydoc@gmail.com>
2023-05-08 10:59:30 +0200

    Added a table

--- components/dashboard/navbar/Navbar.tsx
+++ components/dashboard/navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import NavbarItem from './NavbarItem';
-const Tabs = ["Dashboard", "Invoices", "Clients", "Products", "Settings", "Profile"];
+const Tabs = ["Dashboard", "Invoices", "Customers", "Providers", "Services", "Settings", "Profile"];
 
 
-const Navbar = ({callback}:{callback: any}) => {
+const Navbar = ({ callback }: { callback: any }) => {
     return (
         <div className="flex space-x-10 bg-blue-500 text-slate-200 w-[100vw]">
             {
@@ -11,7 +11,7 @@ const Navbar = ({callback}:{callback: any}) => {
                     return (
                         <div className='hover:scale-105 cursor-pointer'>
                             <div>
-                                <NavbarItem tab={tab} callback={callback}/>
+                                <NavbarItem tab={tab} callback={callback} />
                             </div>
                         </div>
                     )
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from "@/components/dashboard/navbar/Navbar";
 import Footer from '@/components/footer/Footer';
 import ProfileModal from '@/components/dashboard/navbar/profile/ProfileModal';
+import Table from '@/components/dashboard/table/Table';
 
 const Index = () => {
     const [profile, setProfile] = useState(false);
-    if(profile) {
+    if (profile) {
         console.log("#Profile is true");
     }
+    useEffect(() => {
+        //!Todo - Check if user is logged in
+        //!Todo - Check for user data and fetch into a table
+    }, []);
     return (
         <div>
             <header>
                 <title>Nadzorna plošča</title>
                 <div className="flex">
-                   <Navbar callback={setProfile}/> 
+                    <Navbar callback={setProfile} />
                 </div>
             </header>
             <main className='flex flex-col h-screen justify-between'>
                 <div>
-                <div className='flex'>
-                    {profile ? <ProfileModal callback={setProfile} />: null}
+                    <div className='flex flex-col items-center justify-center pt-5 bg-slate-200'>
+                        <Table />
+                    </div>
                 </div>
-                
+
+                <div className='flex flex-col items-center justify-center'>
+                    {profile ? <ProfileModal callback={setProfile} /> : null}
                 </div>
 
                 <footer>
-    <div className=''>
-        <Footer/>
-    </div>
+                    <div className=''>
+                        <Footer />
+                    </div>
 
-        </footer>
+                </footer>
             </main>
         </div>
-        
+
     );
 };
 
