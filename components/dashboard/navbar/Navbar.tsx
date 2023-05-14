9b3cf79caafe2383b6ebd631d1db911793cee1ce
Maj Knez <actuallydoc@gmail.com>
2023-05-14 17:17:10 +0200

    fix

--- components/dashboard/navbar/Navbar.tsx
+++ components/dashboard/navbar/Navbar.tsx
@@ -7,10 +7,10 @@ const Navbar = ({ invoiceCallback, providerCallback, serviceCallback, customerCa
     return (
         <div className="flex pl-10 space-x-10 bg-blue-500 text-slate-200 w-[100vw]">
             {
-                Tabs.map((tab) => {
+                Tabs.map((tab, index) => {
                     return (
 
-                        <div className='flex ml-auto  hover:scale-105 cursor-pointer'>
+                        <div key={index} className='flex ml-auto  hover:scale-105 cursor-pointer'>
                             <div>
                                 <NavbarItem tab={tab} customerCallback={customerCallback} invoiceCallback={invoiceCallback} providerCallback={providerCallback} serviceCallback={serviceCallback} />
                             </div>
