69a32cc1062859f596ac6a72baa5489484d676e9
Maj Knez <actuallydoc@gmail.com>
2023-05-14 17:27:57 +0200

    Fix for vercel

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -15,21 +15,5 @@ export default NextAuth({
         secret: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY as string,
     }),
     secret: process.env.JWT_SECRET,
-    callbacks: {
-        async session({ session, user }) {
-            const signingSecret = process.env.SUPABASE_JWT_SECRET
-            if (signingSecret) {
-                const payload = {
-                    aud: "authenticated",
-                    exp: Math.floor(new Date(session.expires).getTime() / 1000),
-                    sub: user.id,
-                    email: user.email,
-                    role: "authenticated",
-                }
-                session.supabaseAccessToken = jwt.sign(payload, signingSecret)
-            }
-            return session
-        },
-    },
 
 })
